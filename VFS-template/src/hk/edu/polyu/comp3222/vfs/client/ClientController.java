b1f3f1fb8655f4a7005c388b63551325b8c91438
ShingJan <yuanj.shi@hotmail.com>
2017-01-24 09:55:35 +0800

    client and server side program update

---
+++ VFS-template/src/hk/edu/polyu/comp3222/vfs/client/ClientController.java
@@ -0,0 +1,8 @@
+package hk.edu.polyu.comp3222.vfs.client;
+
+/**
+ * Created by Isaac on 1/24/17.
+ */
+public class ClientController {
+    
+}
---
+++ VFS-template/src/hk/edu/polyu/comp3222/vfs/server/ServerController.java
@@ -0,0 +1,7 @@
+package hk.edu.polyu.comp3222.vfs.server;
+
+/**
+ * Created by Isaac on 1/24/17.
+ */
+public class ServerController {
+}
