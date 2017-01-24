27e6a77d897bf267c2c563d1e545ecfa6d1f8a0d
ShingJan <yuanj.shi@hotmail.com>
2017-01-24 10:55:10 +0800

    responsehandler

---
+++ VFS-template/src/hk/edu/polyu/comp3222/vfs/ResponseHandler.java
@@ -0,0 +1,12 @@
+package hk.edu.polyu.comp3222.vfs;
+
+/**
+ * Created by Isaac on 1/24/17.
+ */
+interface ResponseHandler {
+    void handlerResponse(String xxp);
+}
+
+public class ListResponseHandler implements ResponseHandler{
+    public void handlerResponse(String xxp){}
+}
--- VFS-template/src/hk/edu/polyu/comp3222/vfs/client/ClientController.java
+++ VFS-template/src/hk/edu/polyu/comp3222/vfs/client/ClientController.java
@@ -4,5 +4,5 @@ package hk.edu.polyu.comp3222.vfs.client;
  * Created by Isaac on 1/24/17.
  */
 public class ClientController {
-    
+
 }
