30c72ecaed14bd513002e77094b0d35c368d5221
ShingJan <yuanj.shi@hotmail.com>
2017-04-05 15:46:29 +0800

    export command finished

--- VFS-template/src/hk/edu/polyu/comp3222/vfs/handler/ExportResponseHandler.java
+++ VFS-template/src/hk/edu/polyu/comp3222/vfs/handler/ExportResponseHandler.java
@@ -5,6 +5,9 @@ import hk.edu.polyu.comp3222.vfs.core.VFSDirectory;
 import hk.edu.polyu.comp3222.vfs.core.VFSFile;
 import hk.edu.polyu.comp3222.vfs.core.VFSunit;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Isaac on 2/17/17.
  */
@@ -18,15 +21,46 @@ public class ExportResponseHandler extends ResponseHandler{
         VFSunit tempFile = CurrentDir.getItem(cmd[1].split("/"), ioService);
 
         ioService.printLine(tempFile.getClass().toString());
+        if(tempFile.getClass() == VFSDirectory.class){
+            createDirectory((VFSDirectory) tempFile, ioService);
+        }else if(tempFile.getClass() == VFSFile.class){
+            createFile((VFSFile) tempFile, ioService);
+        }
 
         return CurrentDir;
     }
 
-    public void createFile(VFSFile outFile){
-
+    public void createFile(VFSFile outFile, IOService ioService){
+        String tempPath = "host/" + outFile.getPath();
+        File tempFile = new File(tempPath);
+        try {
+            tempFile.getParentFile().mkdirs();
+            tempFile.createNewFile();
+        }catch (IOException e){
+            ioService.printLine("create file failed, action abort.");
+        }
     }
 
-    public void createDirectory(VFSDirectory ourDir){
+    public void createDirectory(VFSDirectory outDir, IOService ioService){
+        String tempPath = "host/" + outDir.getPath();
+        File tempDir = new File(tempPath);
+        if (!tempDir.exists()) {
+            ioService.printLine("creating directory in host/: " + tempDir.getName());
+            boolean result = false;
 
+            try{
+                tempDir.mkdirs();
+                result = true;
+            }
+            catch(SecurityException se){
+                //handle it
+                ioService.printLine("create directory failed");
+            }
+            if(result) {
+                ioService.printLine("DIR created");
+            }
+        }else{
+            ioService.printLine("directory already exists. Operation abort");
+        }
     }
 }
