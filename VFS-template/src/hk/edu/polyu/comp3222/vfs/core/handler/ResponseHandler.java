e2bd8b69d0d6d6bf6c393904ae9c8eb96fde647f
ShingJan <yuanj.shi@hotmail.com>
2017-04-07 19:46:13 +0800

    handler update

--- VFS-template/src/hk/edu/polyu/comp3222/vfs/core/handler/ResponseHandler.java
+++ VFS-template/src/hk/edu/polyu/comp3222/vfs/core/handler/ResponseHandler.java
@@ -4,11 +4,13 @@ import hk.edu.polyu.comp3222.vfs.Util.IOService;
 import hk.edu.polyu.comp3222.vfs.core.vfs.VFSDirectory;
 import hk.edu.polyu.comp3222.vfs.core.vfs.VFSunit;
 
+import java.io.Serializable;
+
 /**
  * Created by Isaac on 1/24/17.
  *
  */
-public abstract class ResponseHandler {
+public abstract class ResponseHandler implements Serializable{
     public abstract VFSunit handlerResponse(String[] cmd, VFSDirectory root, VFSDirectory CurrentDir, IOService ioService);
 }
 
